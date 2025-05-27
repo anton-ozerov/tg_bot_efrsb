015a51ef80b961c8604bda1cf20ec65816f28d5a
Anton Ozerov <ozerovanton17@gmail.com>
2025-05-27 13:35:36 +0300

    added log file to .env

--- app/data/config.py
+++ app/data/config.py
@@ -9,5 +9,5 @@ EFRSB_TOKEN = env.str("EFRSB_TOKEN")
 OSINT_1_TOKEN = env.str("OSINT_1_TOKEN")
 OSINT_2_TOKEN = env.str("OSINT_2_TOKEN")
 OSINT_3_TOKEN = env.str("OSINT_3_TOKEN")
-LOG_FILE = 'bot.log'
+LOG_FILE = env.str('LOG_FILE')
 DB_NAME = env.str('DB_NAME')
